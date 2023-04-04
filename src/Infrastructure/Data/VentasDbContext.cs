c0a8d50b39be03dce968c3e34d7ce6f92945d4a1
MidshipEnd <62455913+MidshipEnd@users.noreply.github.com>
2023-04-03 20:03:54 -0600

    Initial commit

---
+++ src/Infrastructure/Data/VentasDbContext.cs
@@ -0,0 +1,137 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+using Domain;
+
+namespace Infrastructure;
+
+public class VentasDbContext
+{
+    private readonly string _connectionString;
+    public VentasDbContext(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public List<Venta> List()
+    {
+        var data = new List<Venta>();
+
+        var con = new SqlConnection(_connectionString);
+        var cmd = new SqlCommand("SELECT [Id],[ClienteId],[ProductoId], [Fecha] FROM [Venta]", con);
+        try
+        {
+            con.Open();
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                data.Add(new Venta
+                {
+                    Id = (Guid)dr["Id"],
+                    ClienteId = (Guid)dr["ClienteId"],
+                    ProductoId = (Guid)dr["ProductoId"],
+                    Fecha = (DateTime)dr["Fecha"]
+                });
+            }
+            return data;
+        }
+        catch (Exception) 
+        { throw; }
+        finally
+        {
+            con.Close();
+        }
+    }
+
+    public Venta Details(Guid id)
+    {
+        var data = new Venta();
+
+        var con = new SqlConnection(_connectionString);
+        var cmd = new SqlCommand("SELECT [Id],[ClienteId],[ProductoId], [Fecha] FROM [Venta] WHERE Id = @Id", con);
+        cmd.Parameters.Add("@Id", SqlDbType.UniqueIdentifier).Value = id;
+        try
+        {
+            con.Open();
+            var dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                data.Id = (Guid)dr["Id"];
+                data.ClienteId = (Guid)dr["ClienteId"];
+                data.ProductoId = (Guid)dr["ProductoId"];
+                data.Fecha = (DateTime)dr["Fecha"];
+            }
+            return data;
+        }
+        catch (Exception)
+        { throw; }
+        finally
+        {
+            con.Close();
+        }
+    }
+
+    public void Create(Venta data)
+    {
+        var con = new SqlConnection(_connectionString);
+        var cmd = new SqlCommand("INSERT INTO [Venta] ([ClienteId],[ProductoId],[Fecha]) VALUES (@ClienteId,@ProductoId,@Fecha)", con);
+        cmd.Parameters.Add("Id", SqlDbType.UniqueIdentifier).Value = data.Id;
+        cmd.Parameters.Add("ClienteId", SqlDbType.UniqueIdentifier).Value = data.ClienteId;
+        cmd.Parameters.Add("ProductoId", SqlDbType.UniqueIdentifier).Value = data.ProductoId;
+        cmd.Parameters.Add("Fecha", SqlDbType.DateTime).Value = data.Fecha;
+
+
+        try
+        {
+            con.Open();
+            cmd.ExecuteNonQuery();
+        }
+        catch (Exception) 
+        { throw; }
+        finally
+        {
+            con.Close();
+        }
+    }
+
+    public void Edit(Venta data)
+    {
+        var con = new SqlConnection(_connectionString);
+        var cmd = new SqlCommand("UPDATE [Venta] SET [ClienteId] =@ClienteId, [ProductoId] =@ProductoId, [Fecha] =@Fecha) WHERE Id = @Id", con);
+        cmd.Parameters.Add("Id", SqlDbType.UniqueIdentifier).Value = data.Id;
+        cmd.Parameters.Add("ClienteId", SqlDbType.UniqueIdentifier).Value = data.ClienteId;
+        cmd.Parameters.Add("ProductoId", SqlDbType.UniqueIdentifier).Value = data.ProductoId;
+        cmd.Parameters.Add("Fecha", SqlDbType.DateTime).Value = data.Fecha;
+
+        try
+        {
+            con.Open();
+            cmd.ExecuteNonQuery();
+        }
+        catch (Exception) 
+        { throw; }
+        finally
+        {
+            con.Close();
+        }
+    }
+
+    public void Delete(Guid id)
+    {
+        var con = new SqlConnection(_connectionString);
+        var cmd = new SqlCommand("DELETE FROM [Venta] WHERE [Id] = @Id", con);
+        cmd.Parameters.Add("Id", SqlDbType.UniqueIdentifier).Value = id;
+
+        try
+        {
+            con.Open();
+            cmd.ExecuteNonQuery();
+        }
+        catch (Exception) 
+        { throw; }
+        finally
+        {
+            con.Close();
+        }
+    }
+}
---
+++ src/Presentation.WebApp/Controllers/VentasController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+using Domain;
+using Infrastructure;
+
+namespace Presentation.WebApp.Controllers;
+
+public class VentasController : Controller
+{
+    private readonly VentasDbContext _ventasDbContext;
+    private readonly ClientesDbContext _clientesDbContext;
+    private readonly ProductosDbContext _productosDbContext;
+    public VentasController(IConfiguration configuration)
+    {
+        _ventasDbContext = new VentasDbContext(configuration.GetConnectionString("DefaultConnection"));
+        _clientesDbContext = new ClientesDbContext(configuration.GetConnectionString("DefaultConnection"));
+         _productosDbContext = new ProductosDbContext(configuration.GetConnectionString("DefaultConnection"));
+    }
+
+    public IActionResult Index()
+    {
+        var data = _ventasDbContext.List();
+        return View(data);
+    }
+
+    public IActionResult Details(Guid id)
+    {
+        var data = _ventasDbContext.Details(id);
+        return View(data);
+    }
+
+    public IActionResult Create()
+    {
+        ViewBag.Cliente = _clientesDbContext.List();
+        ViewBag.Producto = _productosDbContext.List();
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Create(Venta data)
+    {
+        _ventasDbContext.Create(data);
+        //SmtpClientEmailService.SendEmail(data.Cliente.Correo, "Asunto", $"<h4>Hola {data.Cliente.Nombre}</h1>", true);
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult Edit(Guid id)
+    {
+        var data = _ventasDbContext.Details(id);
+        ViewBag.Cliente = _clientesDbContext.List();
+        ViewBag.Producto = _productosDbContext.List();
+        return View(data);
+    }
+    [HttpPost]
+    public IActionResult Edit(Venta data)
+    {
+        _ventasDbContext.Edit(data);
+        ViewBag.Cliente = _clientesDbContext.List();
+        ViewBag.Producto = _productosDbContext.List();
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult Delete(Guid id)
+    {
+        _ventasDbContext.Delete(id);
+        return RedirectToAction("Index");
+    }
+}
