f68be8813ef3dd7d950fb9aad0eb42ea8fa1a9f4
MidshipEnd <62455913+MidshipEnd@users.noreply.github.com>
2023-04-17 19:39:09 -0600

    ya
    a

---
+++ src/Domain/Entities/Venta.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain;
+public class Venta
+{
+    public Guid Id { get; set; }
+
+    //Cliente
+    public Guid ClienteId { get; set; }
+
+    //Producto
+    public Guid ProductoId { get; set; }
+    
+    public DateTime Fecha { get; set; }
+}
+
